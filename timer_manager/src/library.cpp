c884a3084a425e306b95d352c83810673a716d5b
DSafr2506 <122727485+DSafr2506@users.noreply.github.com>
2025-03-22 13:51:11 +0300

    Update library.cpp

--- timer_manager/src/library.cpp
+++ timer_manager/src/library.cpp
@@ -4,20 +4,17 @@ namespace template_library {
 
     void EventScheduler::add_event(int time, const std::string& event) {
         events[time] = event;
-        event_times.push(time);
+        time_queue.push(time);
     }
 
     std::string EventScheduler::process_next() {
-        if (event_times.empty()) {
+        if (time_queue.empty()){
             return "";
         }
-
-        int next_time = event_times.top();
-        event_times.pop();
-
-        std::string next_event = events[next_time];
-        events.erase(next_time);
-
-        return next_event;
+        else{
+            auto temp = events.find(time_queue.top()) -> second;
+            time_queue.pop();
+            return temp;
+        }
     }
 }
