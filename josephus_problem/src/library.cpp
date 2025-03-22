ed2cf380fea9fd02c738df962893de96eee957e8
DSafr2506 <122727485+DSafr2506@users.noreply.github.com>
2025-03-22 13:58:39 +0300

    Add files via upload

---
+++ josephus_problem/src/library.cpp
@@ -0,0 +1,23 @@
+
+#include "library.hpp"
+
+namespace template_library {
+    int josephus_problem([[maybe_unused]] const std::vector<int>& nums,
+                         [[maybe_unused]] int k) {
+        std::deque<int> dq; 
+        int a=nums.size();
+        for (int i=0; i<a; ++i){
+            dq.push_back(nums[i]);
+        }
+        while (dq.size() > 1) {
+            int temp = (k - 1) % dq.size();
+            for (int i = 0; i < temp; ++i) {
+                dq.push_back(dq.front());
+                dq.pop_front();
+            }
+            dq.pop_front(); // Удаление K-го элемента
+        }
+    return dq.front();
+        return 0;
+    };
+}
